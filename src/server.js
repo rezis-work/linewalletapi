a89ffa2e36fc0fbd8f1e9e4141f2238c46d86d01
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-08-15 20:57:15 +0400

    cron added for running server

--- src/server.js
+++ src/server.js
@@ -4,19 +4,25 @@ import rateLimiter from "./middleware/rate-limiter.js";
 import { initDB } from "./config/db.js";
 import transactionsRouter from "./routes/transactions.route.js";
 import ratelimit from "./config/upstash.js";
+import job from "./config/cron.js";
 
 dotenv.config();
 
 const app = express();
 
+if (process.env.NODE_ENV === "production") {
+  job.start();
+}
+
 app.use(rateLimiter);
 app.use(express.json());
 
-app.get("/health", async (req, res) => {
+app.get("/api/health", async (req, res) => {
   const key = `${req.ip}:${req.path}`;
   const result = await ratelimit.limit(key);
   res.status(200).json({
     message: "Server is running",
+    status: "ok",
     status: "healthy",
     ip: req.ip,
     result,
